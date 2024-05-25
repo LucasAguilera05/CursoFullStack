b0d11481e60403097cd07a7a78d67d03b89ded28
LucasAguilera05 <lucasaguilera0506@gmail.com>
2024-05-25 04:15:31 -0300

    parte1 del proyect

---
+++ parte1/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Mensaje from './Mensaje';
+
+function App() {
+  
+  return (
+    <div className="App">
+
+  <Mensaje color='blue' message='estamos trabajando'/>     
+      
+    </div>
+  );
+}
+
+export default App;
